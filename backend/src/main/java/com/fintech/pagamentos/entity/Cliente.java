b1d35059b13cbd8e6a3e325d3017b730a4ca01bc
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-07-11 13:06:30 -0300

    feat: adiciona validação usando jakarta.validation nas entidades Cliente e Fatura

--- backend/src/main/java/com/fintech/pagamentos/entity/Cliente.java
+++ backend/src/main/java/com/fintech/pagamentos/entity/Cliente.java
@@ -3,6 +3,7 @@ package com.fintech.pagamentos.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -23,18 +24,29 @@ public class Cliente {
     @JdbcTypeCode(SqlTypes.VARCHAR)
     private UUID id;
 
+    @NotBlank(message = "O nome é obrigatório.")
+    @Size(max = 100, message = "O nome deve ter no máximo 100 caracteres.")
     @Column(nullable = false, length = 100)
     private String nome;
 
+    @NotBlank(message = "O CPF é obrigatório.")
+    @Size(min = 11, max = 11, message = "O CPF deve ter exatamemte 11 dígitos.")
     @Column(nullable = false, unique = true, length = 11)
     private String cpf;
 
+    @NotNull(message = "A data nascimento é obrigatória.")
+    @Past(message = "A data de nascimento deve ser uma data no passado.")
     @Column(name = "data_nascimento", nullable = false)
     private LocalDate dataNascimento;
 
+    @NotNull(message = "O status de bloqueio é obrigatório.")
+    @Size(min = 1, max = 1, message = "O status de bloqueio só pode ter um caracter.")
+    @Pattern(regexp = "[AB]", message = "O status de bloqueio de ser 'A' (Ativo) ou 'B' (Bloqueado).")
     @Column(name = "status_bloqueio", nullable = false, length = 1)
     private Character statusBloqueio; // 'A' para Ativo, 'B' para Bloqueado
 
+    @NotNull(message = "O limite de crédito é obrigatório.")
+    @DecimalMin(value = "0.00", inclusive = true, message = "O limite de crédito não pode ser negativo.")
     @Column(name = "limite_credito", nullable = false, precision = 10, scale = 2)
     private BigDecimal limiteCredito;
 
--- backend/src/main/java/com/fintech/pagamentos/entity/Fatura.java
+++ backend/src/main/java/com/fintech/pagamentos/entity/Fatura.java
@@ -1,6 +1,7 @@
 package com.fintech.pagamentos.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,19 +22,28 @@ public class Fatura {
     @JdbcTypeCode(SqlTypes.VARCHAR)
     private UUID id;
 
+    @NotNull(message = "A fatura deve estar associada a um cliente.")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cliente_id", nullable = false)
     private Cliente cliente;
 
+    @NotNull(message = "A data de vencimento é obrigatória.")
+    @FutureOrPresent(message = "A data de vencimento deve ser igual ou posterior à data atual.")
     @Column(name = "data_vencimento", nullable = false)
     private LocalDate dataVencimento;
 
+    @PastOrPresent(message = "A data de pagamento não pode ser uma data futura.")
     @Column(name = "data_pagamento")
     private LocalDate dataPagamento;
 
+    @NotNull(message = "O valor da fatura é obrigatório.")
+    @DecimalMin(value = "0.01", message = "O valor da fatura deve ser maior que zero.")
     @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal valor;
 
+    @NotNull(message = "O status da fatura é obrigatório")
+    @Size(min = 1, max = 1, message = "O status da fatura só pode ter um caracter.")
+    @Pattern(regexp = "[PAB]", message = "O status da fatura deve ser 'P' (Paga), 'A' (Atrasada) ou 'B' (Aberta).")
     @Column(nullable = false, length = 1)
     private Character status;
 
