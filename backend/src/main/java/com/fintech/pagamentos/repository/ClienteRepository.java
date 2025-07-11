94d3c4c4fa2a96de4c9bd06ee2a308c049c375bf
Cauany Nunes Rodrigues <cauanyrodrigues01@gmail.com>
2025-07-11 17:58:25 -0300

    feat(ClienteRepository): adiciona função para encontrar pelo status bloqueio de cliente

--- backend/src/main/java/com/fintech/pagamentos/repository/ClienteRepository.java
+++ backend/src/main/java/com/fintech/pagamentos/repository/ClienteRepository.java
@@ -4,7 +4,11 @@ package com.fintech.pagamentos.repository;
 import com.fintech.pagamentos.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ClienteRepository extends JpaRepository<Cliente, UUID> {
+
+    List<Cliente> findByStatusBloqueio(Character statusBloqueio);
+
 }
